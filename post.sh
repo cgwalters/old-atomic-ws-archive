19e4386e50a521d7d2533617fd0d14f87f175ed1
Colin Walters <walters@verbum.org>
2017-04-18 15:50:27 -0400

    post: Use overlay2, not overlay
    This was an over[lay]sight on my part.

--- post.sh
+++ post.sh
@@ -8,5 +8,5 @@ echo 'Storage=persistent' >> /etc/systemd/journald.conf
 # Work around https://github.com/systemd/systemd/issues/4082
 find /usr/lib/systemd/system/ -type f -exec sed -i -e '/^PrivateTmp=/d' -e '/^Protect\(Home\|System\)=/d' {} \;
 
-# Docker initialization to use overlayfs (and disable selinux for now)
-echo 'STORAGE_DRIVER=overlay' >> /etc/sysconfig/docker-storage-setup
+# Early: https://fedoraproject.org/wiki/Changes/DockerOverlay2
+echo 'STORAGE_DRIVER=overlay2' >> /etc/sysconfig/docker-storage-setup
